cbf66cc48f741c01c84ef16d5eec68904e35a444
ClementAver <clement.aver@yahoo.fr>
2022-12-13 23:35:00 +0100

    progresses on entries validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,8 +30,51 @@ function closeModal() {
 // closes modal event
 modalCross.addEventListener("click", closeModal);
 
-// fisrt and last name verification
-//place vérification fonctions here
+// test the length of a string
+function testLength(string) {
+  if (string.length >= 2) {
+    console.log(`${string} est valide`);
+    return true;
+  } else {
+    console.log(`${string} est invalide`);
+    return false;
+  }
+}
+
+// test email
+function testEmail(email) {
+  // regex : "RFC2822 Email Validation" by Tripleaxis on regexr.com
+  const regExpMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  if (regExpMail.test(email)) {
+    console.log(`${email} est une adresse valide`);
+    return true;
+  } else {
+    console.log(`${email} n'est pas une adresse valide`);
+    return false;
+  }
+}
+
+//test number
+function testNumber(number) {
+  const regExpNumber = /[0-9]/;
+  if (regExpNumber.test(number)) {
+    console.log(`${number} est un nombre valide`);
+    return true;
+  } else {
+    console.log(`${number} n'est pas un nombre valide`);
+    return false;
+  }
+}
+
+//test exist
+function testExist(being) {
+  if (being) {
+    return true;
+  } else {
+    console.log(`${being} does not exist`);
+    return false;
+  }
+}
 
 // launch submit
 function submitModal(e) {
@@ -39,23 +82,25 @@ function submitModal(e) {
 
   // inputs values
   let first = document.getElementById("first").value;
-  console.log(first);
   let last = document.getElementById("last").value;
-  console.log(last);
   let email = document.getElementById("email").value;
-  console.log(email);
   let birthdate = new Date(document.getElementById("birthdate").value);
-  console.log(birthdate);
-  let quantity = +document.getElementById("quantity").value;
-  console.log(quantity);
-  let tournament = document.querySelector("#tournament input:checked").value;
-  console.log(tournament);
+  let quantity = document.getElementById("quantity").value;
+  let tournament = document.querySelector("#tournament input:checked");
   let checkbox1 = document.getElementById("checkbox1").checked;
-  console.log(checkbox1);
   let checkbox2 = document.getElementById("checkbox2").checked;
-  console.log(checkbox2);
 
-  //place verifications here
+  // verifications
+  if (testLength(first) && testLength(last) && testEmail(email) && testNumber(quantity) && testExist(tournament)) {
+    console.log(first);
+    console.log(last);
+    console.log(email);
+    console.log(birthdate);
+    console.log(quantity);
+    console.log(tournament);
+    console.log(checkbox1);
+    console.log(checkbox2);
+  }
 }
 
 // submit event
