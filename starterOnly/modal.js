e1a6b479381a05e7930555bddccac52f9ab61c1e
ClementAver <clement.aver@yahoo.fr>
2022-12-15 13:37:11 +0100

    fixes issues #3 and #4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,114 +8,153 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const btnSubmit = document.querySelector(".modal-body .btn-submit");
+const modalbg = document.querySelectorAll(".bground");
+const reserveForm = document.getElementById("reserve");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const modalCross = document.querySelector(".close");
+const modalCross = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
+const invalidMessages = document.querySelectorAll(".invalid-fields");
+const validationFeedback = document.getElementById("validation-message");
+console.log(modalBtn);
+//inputs
+const firstNAme = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
 
-// launches modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// launches modals
+function launchModal(element, index) {
+  element[index].style.display = "block";
 }
 
-// launches modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launches modal form
-function closeModal() {
-  modalbg.style.display = "none";
+// closes modals
+function closeModal(element, index) {
+  element[index].style.display = "none";
 }
 
-// closes modal event
-modalCross.addEventListener("click", closeModal);
+// launches modal form event
+modalBtn[0].addEventListener("click", function () {
+  launchModal(modalbg, 0);
+});
+modalBtn[1].addEventListener("click", function () {
+  launchModal(modalbg, 0);
+});
 
-// test the length of a string
+// closes modal form event
+modalCross[0].addEventListener("click", function () {
+  closeModal(modalbg, 0);
+});
+
+// closes modal validation event
+modalCross[1].addEventListener("click", function () {
+  closeModal(modalbg, 1);
+});
+modalBtn[2].addEventListener("click", function () {
+  closeModal(modalbg, 1);
+});
+
+// tests the length of a string
 function testLength(string) {
-  if (string.length >= 2) {
-    console.log(`${string} est valide`);
-    return true;
-  } else {
-    console.log(`${string} est invalide`);
-    return false;
-  }
+  return string.length >= 2;
 }
 
-// test email
-function testEmail(email) {
+// tests email
+function isEmail(email) {
   // regex : "RFC2822 Email Validation" by Tripleaxis on regexr.com
   const regExpMail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-  if (regExpMail.test(email)) {
-    console.log(`${email} est une adresse valide`);
-    return true;
-  } else {
-    console.log(`${email} n'est pas une adresse valide`);
-    return false;
-  }
+  return regExpMail.test(email.value);
 }
 
-//test number
-function testNumber(number) {
-  const regExpNumber = /[0-9]/;
-  if (regExpNumber.test(number)) {
-    console.log(`${number} est un nombre valide`);
-    return true;
-  } else {
-    console.log(`${number} n'est pas un nombre valide`);
+// tests date
+function isDate(date) {
+  if (new Date(date.value) == "Invalid Date") {
     return false;
-  }
-}
-
-// test date
-function testDate(date) {
-  if (date == "Invalid Date") {
-    console.log(`${date} n'est pas une date valide`);
-    return false;
-  } else if (typeof date === "object") {
-    console.log(`${date} est une date valide`);
+  } else if (typeof new Date(date.value) === "object") {
     return true;
   }
 }
 
-//test exist
-function testTournament(tournament) {
-  if (tournament) {
-    console.log(`${tournament.value} est sélectionné`);
-    return true;
-  } else {
-    console.log(`aucun tournoi sélectionné`);
-    return false;
-  }
+// tests number
+function isNumber(number) {
+  const regExpNumber = /^[0-9]{1,2}/;
+  return regExpNumber.test(number.value);
 }
 
+// tests if checked
 function testCheck(checkbox) {
-  if (checkbox.checked) {
-    console.log(`${checkbox.id} est cochée`);
+  return checkbox.checked;
+}
+
+function isNotNull(variable) {
+  return variable != null;
+}
+
+//validates inputs
+function isValid(callback, argument, index) {
+  if (callback(argument)) {
+    invalidMessages[index].classList.remove("invalid-field");
     return true;
   } else {
-    console.log(`${checkbox.id} n'est pas cochée`);
+    invalidMessages[index].classList.add("invalid-field");
+    throw new Error(`The field n°${index} isn't valid`);
+  }
+}
+
+// checks if all inputs are valid
+function allValid() {
+  const wishedTournament = document.querySelector("#tournament .checkbox-input:checked");
+  try {
+    //isValid(testLength, first.value, 0);
+    //isValid(testLength, last.value, 1);
+    //isValid(isEmail, email, 2);
+    //isValid(isDate, birthdate, 3);
+    //isValid(isNumber, quantity, 4);
+    //isValid(isNotNull, wishedTournament, 5);
+    isValid(testCheck, checkbox1, 6);
+  } catch (err) {
+    var error = err;
+    console.error(err);
+  }
+
+  if (!error) {
+    return true;
+  } else {
     return false;
   }
 }
-// launch submit
-function submitModal(e) {
+
+// launches submit
+function validate(e) {
   e.preventDefault();
 
-  // inputs
-  let first = document.getElementById("first").value;
-  let last = document.getElementById("last").value;
-  let email = document.getElementById("email").value;
-  let birthdate = new Date(document.getElementById("birthdate").value);
-  let quantity = document.getElementById("quantity").value;
-  let tournament = document.querySelector("#tournament input:checked");
-  let checkbox1 = document.getElementById("checkbox1");
-  let checkbox2 = document.getElementById("checkbox2");
+  if (allValid()) {
+    closeModal(modalbg, 0);
+    launchModal(modalbg, 1);
+    wishedTournament = document.querySelector("#tournament .checkbox-input:checked");
 
-  // verifications
-  if (testLength(first) && testLength(last) && testEmail(email) && testDate(birthdate) && testNumber(quantity) && testTournament(tournament) && testCheck(checkbox1)) {
-    testCheck(checkbox2);
+    fetch("", {
+      method: "POST",
+      headers: JSON.stringify({
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        name: first.value,
+        surname: last.value,
+        email: email.value,
+        birthdate: new Date(birthdate.value),
+        quantity: +quantity.value,
+        tournament: wishedTournament.value,
+        newsletter: checkbox2.checked,
+      }),
+    });
+  } else {
+    console.log("le formulaire n'est valide");
   }
 }
 
 // submit event
-btnSubmit.addEventListener("click", submitModal);
+reserveForm.addEventListener("submit", validate);
